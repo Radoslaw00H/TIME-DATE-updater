215a1462209a5e7cf2d051eee77d0f36bebc5886
Radoslaw00H <holubowskir@gmail.com>
2026-03-03 18:24:51 +0100

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,22 @@
+// Time and date program
+
+#include <stdint.h>	// Integers library
+#include <stdio.h>	// Input/Output library
+
+void dayMonthYear(void) {
+// A struct of dates
+	struct Date {
+	uint8 day;
+	uint8 month;
+	uint16 year;
+	};
+// Todays date
+	struct Date today = {3, 3, 2026}
+}
+
+int main(void) {
+	while(1) {
+	// funkcje na potem
+	}
+return 0;
+}
