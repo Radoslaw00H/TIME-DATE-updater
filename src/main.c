e02d0e351cd2b16ccc229d438a0dfd09c1993684
Radoslaw00H <holubowskir@gmail.com>
2026-03-03 19:02:42 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,15 +3,24 @@
 #include <stdint.h>	// Integers library
 #include <stdio.h>	// Input/Output library
 
-void dayMonthYear(void) {
+void DateTime(void) {
 // A struct of dates
 	struct Date {
 	uint8 day;
 	uint8 month;
 	uint16 year;
 	};
+// A struct of time
+	struct Time {
+	uint8 hour;
+	uint8 minute;
+	uint8 second;
+	uint8 milisecond;
+	};
 // Todays date
 	struct Date today = {3, 3, 2026}
+// Time now (i'll need to figure it out somehow
+	struct Time now = {18, 49, 13, 2}
 }
 
 int main(void) {
